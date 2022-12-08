70c5342725df1734662d00b9cefa83539dc8e2f5
HristoE <114247445+HristoE@users.noreply.github.com>
2022-12-08 14:25:42 -0500

    String Id is not declared as astatic which throws an error

---
+++ src/sprint1/Test.java
@@ -0,0 +1,13 @@
+package sprint1;
+
+public class Test {
+
+    static String name = "John";
+   static  String id = "123";
+
+
+    public static void main(String[] args) {
+        System.out.println(name);
+        System.out.println(id + 1);
+    }
+}
