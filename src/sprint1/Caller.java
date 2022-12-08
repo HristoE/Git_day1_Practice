7870fc8242460760064434751f2a598ec3957635
HristoE <114247445+HristoE@users.noreply.github.com>
2022-12-08 14:21:19 -0500

    new test added

---
+++ src/sprint1/Caller.java
@@ -0,0 +1,12 @@
+package sprint1;
+
+public class Caller {
+    public void init (){
+        System.out.println("Initialization");
+    }
+    public void start (){
+        init();
+        System.out.println("Started");
+    }
+}
+
---
+++ src/sprint1/TestCall.java
@@ -0,0 +1,9 @@
+package sprint1;
+
+public class TestCall {
+    public static void main(String[] args) {
+        Caller c = new Caller();
+        c.start();
+        c.init();
+    }
+}
